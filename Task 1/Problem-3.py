7a8243fa1121e8091a5fd84f79e74e572f8f9210
Hiral Vaghela <95035313+hiralvaghela@users.noreply.github.com>
2022-07-25 21:18:34 +0530

    Add files via upload

---
+++ Task 1/Problem-3.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum = a + b
+    print(sum)
+    
+    div = a - b
+    print(div)
+    
+    product = a * b
+    print(product)
---
+++ Task 1/Problem-4.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    div_1  = a//b
+    print(div_1)
+    
+    div_2 = a/b
+    print(div_2)
